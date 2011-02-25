b905fef7869bcdbb19792f2f6723d163f4ef268d
Dave Dash <dd+github@davedash.com>
2011-02-24 16:14:18 -0800

    Adding chunked to celeryutils

--- celeryutils.py
+++ celeryutils.py
@@ -39,3 +39,24 @@ def task(*args, **kw):
         return decorate(*args)
     else:
         return decorate
+
+
+def chunked(seq, n):
+    """
+    Yield successive n-sized chunks from seq.
+
+    >>> for group in chunked(range(8), 3):
+    ...     print group
+    [0, 1, 2]
+    [3, 4, 5]
+    [6, 7]
+
+    Useful for feeding celery a list of n items at a time.
+    """
+    seq = iter(seq)
+    while 1:
+        rv = list(itertools.islice(seq, 0, n))
+        if not rv:
+            break
+        yield rv
+
